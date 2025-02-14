e41a106554c9d2a4af883212c8f828217d21cf42
irolram <ivanr9016@gmail.com>
2025-02-14 21:33:42 +0100

    Falta el main

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,20 @@
 
 fun main() {
 
+        fun comprobarPrestamo(usuario: Usuario, libro: Libro){
+            when(usuario){
+                is Usuario.Estudiante -> println("Prestamo de libro ${libro.titulo} a ${usuario.nombre} aceptado")
+                is Usuario.Profesor -> println("Prestamo ${libro.titulo} a ${usuario.nombre} aceptado")
+                is Usuario.Visitante -> println("Prestamo ${libro.titulo} a ${usuario.nombre} denegado")
+            }
+        }
+
+    val usuario1 = Usuario.Profesor("3245356", "Pepe", "Cocina")
+    val usuario2 = Usuario.Visitante("49835", "Juan")
+    val usuario3 = Usuario.Estudiante("589576", "Luisa", "Periodismo")
+
+    val libro1 = Libro("Noseque", "Nosecuanto")
+
+    
+
 }
--- src/Usuario.kt
+++ src/Usuario.kt
@@ -4,14 +4,5 @@ sealed class Usuario {
     class Profesor(val id: String, val nombre:String, val departamento:String): Usuario()
     class Visitante(val id: String, val nombre:String): Usuario()
 
-    companion object{
-        fun comprobarPrestamo(usuario: Usuario, libro: Libro){
-            when(usuario){
-                is Usuario.Estudiante -> println("Prestamo aceptado")
-                is Usuario.Profesor -> println("Prestamo aceptado")
-                is Usuario.Visitante -> println("Prestamo denegado")
-            }
-        }
-    }
 
 }
